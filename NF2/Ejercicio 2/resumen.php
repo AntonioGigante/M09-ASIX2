20372fcc7cb4904dd99f8f90c986e70a03533291
Anotonio Gigante Arias <tonigigantearias@gmail.com>
2022-02-15 20:05:10 +0100

    Ejercicio2 tablas

--- NF2/Ejercicio 2/resumen.php
+++ NF2/Ejercicio 2/resumen.php
@@ -3,11 +3,48 @@
     <head>
         <meta charset="utf-8">
         <title>envio</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     </head>
     <body>
         <h1>Gestión de compra - resumen</h1>
-        <table>
-            
+        <table class="table table-bordered table-dark">
+            <thead>
+            <tr>
+                <td colspan="3">Detalles de Pedido</td>
+            </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td rowspan="2">HW</td>
+                    <td>procesador</td>
+                    <td>precio</td>
+                </tr>
+                <tr>
+                    <td>RAM</td>
+                    <td>precio</td>
+                </tr>
+                <tr>
+                    <td colspan="2">Total</td>
+                    <td>€</td>
+                </tr>
+                <thead>
+                    <tr>
+                        <td colspan="3">Datos de envio</td>
+                    </tr>
+                    <tr>
+                        <td>Nombre:</td>
+                        <td colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td>Direccion:</td>
+                        <td colspan="2"></td>
+                    </tr>
+                    <tr>
+                        <td>Telefono:</td>
+                        <td colspan="2"></td>
+                    </tr>
+                </thead>
+            </tbody> 
         </table>
     </body>
 </html>
